a32fcfe682a8ad35795788a53ed8d4a8f19113c6
Aziz <azizmiftakhul19@gmail.com>
2022-03-15 20:17:55 +0700

    update main package

---
+++ src/main/java/coding_interview/main.java
@@ -0,0 +1,8 @@
+package coding_interview;
+
+public class main {
+    public static void main(String Args []) {
+        System.out.println("Hello this main coding interview, for find code go to test package coding interview");
+        System.out.println("thank all");
+    }
+}
