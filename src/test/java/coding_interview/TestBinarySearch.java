a26f6798926c873b0684d8691df1eb5fb4d52f79
Aziz <azizmiftakhul19@gmail.com>
2022-03-15 20:43:33 +0700

    update test case binarysearch

--- src/test/java/coding_interview/TestBinarySearch.java
+++ src/test/java/coding_interview/TestBinarySearch.java
@@ -1,5 +1,8 @@
 package coding_interview;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
 public class TestBinarySearch {
@@ -23,5 +26,14 @@ public class TestBinarySearch {
             }
         }
         return -1;
+    }
+
+    @Test
+    public void testBinarySearch() {
+        System.out.println(BinarySearch(new int[] {1,2,4,22,55,77,91,80}, 91));
+
+        int target_actual = 7;
+        
+        Assertions.assertEquals(BinarySearch(new int[] {1,2,4,22,55,77,91,80}, 91), target_actual);
     }
 }
