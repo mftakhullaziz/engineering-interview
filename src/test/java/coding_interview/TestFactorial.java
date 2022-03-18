91f85e6fb0d72ebac57205eef392fbbfedf02d03
Aziz <azizmiftakhul19@gmail.com>
2022-03-18 18:56:45 +0700

    update code factorial case

--- src/test/java/coding_interview/TestFactorial.java
+++ src/test/java/coding_interview/TestFactorial.java
@@ -1,4 +1,12 @@
 package coding_interview;
 
 public class TestFactorial {
+    // Kompleksitas Linier O(N)
+    public int FactorialCase(int value){
+        int temp = 1;
+        for (int i = value; i >=1 ; i--) {
+            temp *= i;
+        }
+        return temp;
+    }
 }
