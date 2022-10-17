640112487fe2518f0b3603ca69dad1a9050fabcc
amifth <miftakhul.14116078@student.itera.ac.id>
2022-10-17 21:47:53 +0700

    update

--- src/test/java/backend_interview/BinarySearchTest.java
+++ src/test/java/backend_interview/BinarySearchTest.java
@@ -7,14 +7,13 @@ import java.util.Arrays;
 
 public class BinarySearchTest {
     public int BinarySearch(int[] array_n, int target) {
-        // init left and right
+
         int left = 0;
         int right = array_n.length-1;
 
         // sort array
         Arrays.sort(array_n);
 
-        // check while left <= right
         while (left <= right) {
             int mid = (left + right) / 2;
             if (array_n[mid] == target) {
@@ -29,11 +28,10 @@ public class BinarySearchTest {
     }
 
     @Test
-    public void testBinarySearch() {
-        System.out.println(BinarySearch(new int[] {1,2,4,22,55,77,91,80}, 91));
-
+    void binarySearchTest() {
+        // System.out.println(BinarySearch(new int[] {1,2,4,22,55,77,91,80}, 91));
         int target_actual = 7;
-
         Assertions.assertEquals(BinarySearch(new int[] {1,2,4,22,55,77,91,80}, 91), target_actual);
     }
+
 }
